50e0347588c0439aa0316aaf5f93e0aa83f313dd
hoangson.101197 <hoangson.101197@gmail.com>
2021-07-01 16:02:57 +0700

    Building the models and make initialmigration

---
+++ store/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True)
+
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    # on_delete - CASCADE: khi Category liên quan bị xóa toàn bộ các product liên quan cũng bị xóa
+    category = models.ForeignKey(Category, related_name='product', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='product_creator')
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255, default='admin')
+    description = models.TextField(blank=True)
+    # ImageField không thực sự lưu hình ảnh mà là lưu liên kết tới hình ảnh
+    image = models.ImageField(upload_to='images/')
+    slug = models.SlugField(max_length=255)
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+    in_stock = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+    class Meta:
+        verbose_name_plural: 'Products'
+        ordering = ('-created',),
+
+    def __str__(self):
+        return self.title
